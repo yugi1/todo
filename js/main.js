8dc6733cbcce7fd49d37cafa404866a5448dde4e
yulissa <yulissagil113@gmail.com>
2021-03-05 09:32:52 -0700

    start of js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const lists = {
+    1: {
+      name: "Lists",
+      todos: [
+        {
+          text: 'todo1',
+          completed: false
+        },
+        {
+          text: 'todo',
+          completed: false
+        }
+      ]
+    },
+   }
+   const currentList = lists[0];
+   
+   
+   
