c72b40b9573ef6d25d8807309d04e6ab21f55f2c
yulissa <yulissagil113@gmail.com>
2021-03-09 02:13:43 -0700

    trying to append

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
-const lists = {
-  1: {
-    name: "Shopping list",
-    todos: [
-      {
-        text: 'bananas',
-        completed: false
-      },
-      {
-        text: '1 lbs ground turkey',
-        completed: false
-      }
-    ]
-  },
- }
- const currentList = lists[0];
- 
 
-//print out created lists
-function printLists() {
-  let holder = '<ul class="holdingL">';
-  lists.forEach((list) => {
-    listsHtml += `<li class="listN">${list.name}</li>`;
-  });
- 
-  holder += '</ul>';
-  document.getElementById('lists').innerHTML = listsHtml;
-
-}
 //react to user input for lists
+//function addList() {
+  //const input = document.getElementById('lName').value;
+   //let reactInput = document.getElementById("lol").innerHTML = `<li class="list-group-item">${input}</li>`;
+    
+//}
 function addList() {
-  const input = document.getElementById('lName').value;
-  if(input) {
-    document.getElementById("lol").innerHTML = `<li class="list-item">${input}</li>`;
-    render();
-  }
+  let input = document.getElementById('lName').value;
+   let reactInput = document.getElementById("lol").innerHTML = `<li class="list-group-item">${input}</li>`;
 }
