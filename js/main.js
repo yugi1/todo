c48f05bb2612973e74438e87348e7fadece504fe
yulissa <yulissagil113@gmail.com>
2021-03-10 01:03:05 -0700

    starting js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 
-//react to user input for lists
-//function addList() {
-  //const input = document.getElementById('lName').value;
-   //let reactInput = document.getElementById("lol").innerHTML = `<li class="list-group-item">${input}</li>`;
-    
-//}
+
 function addList() {
   let input = document.getElementById('lName').value;
-   let reactInput = document.getElementById("lol").innerHTML = `<li class="list-group-item">${input}</li>`;
+  let adding = document.createElement("LI");
+  adding.innerHTML = `${input}`;
+  document.body.appendChild(adding);
+}
+//function edit() {
+ // let editbtn = document.createElement("BUTTON");   // Create a <button> element
+//editbtn.innerHTML = "CLICK ME";                   // Insert text
+// document.body.appendChild(editbtn); 
+//}
+let closing = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < closing.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+closing[i].appendChild(span);
 }
