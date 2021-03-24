a3c3575d7b49f2efb22a0a8453b715a9c69388f5
yulissa <yulissagil113@gmail.com>
2021-03-24 12:57:19 -0600

    checkboxes and buttons

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@
 function addList() {
   let input = document.getElementById('lName').value;
   let adding = document.createElement("LI");
-  adding.innerHTML = `${input}`;
+  //adding checkbox 
+  adding.innerHTML = `${input}` + "<span class='editB'>Edit</span>" + "<span class='closeB'>\u00D7</span>";
   whereadd.appendChild(adding);
 }
 
 function toggleWindow() {
   document.getElementById("headerh").style.display = 'block';
 }
-
-//putting todo content on seperate lists
 
 document.getElementById("lol").addEventListener("click", todos);
 
@@ -20,16 +19,13 @@ let whereadd2 = document.getElementById('tabContent2');
 function todoList() {
   let value = document.getElementById('todoAdd').value;
 let adding2 = document.createElement("LI");
-adding2.innerText = `${value}`;
+adding2.innerHTML = `${value}` + "<input type='checkbox' id='checkB'>" + "<span class='editB2'>Edit</span>" + "<span class='closeB2'>\u00D7</span>";
 whereadd2.appendChild(adding2);
 }
 
 function todos() {
   document.getElementById("tabContent").innerHTML = "<input type='text' id='todoAdd' placeholder='To Do...'><input type='submit' id='todoB' value='Add To Do' onclick='todoList()'>";
 } 
-
-
-
 
 
 //adding the delete button
