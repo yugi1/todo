ee63a6db1760c31c8ff4d8b7339a8c7e681037f9
yulissa <yulissagil113@gmail.com>
2021-04-10 02:18:44 -0600

    more hs

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ let whereadd2 = document.getElementById('tabContent2');
 function todoList() {
   let value = document.getElementById('todoAdd').value;
 let adding2 = document.createElement("LI");
-adding2.setAttribute("id", "clickMe2");
+adding2.setAttribute("id", "clickMe2"); 
 adding2.innerHTML = `${value}` + "<input type='checkbox' id='checkB'>" + "<span class='editB2'>Edit</span>" + "<span class='closeB2'>\u00D7</span>";
 whereadd2.appendChild(adding2);
 
@@ -29,18 +29,19 @@ for (let i = 0; i < closeBtn2.length; i++) {
     parent.style.display = "none";
   }
 }
+
+//place code here
+let editBtn = document.getElementsByClassName('editB2');
+for (let i = 0; i < editBtn.length; i++) {
+  editBtn[i].onclick = function () {
+    let newInput = document.getElementById('clickMe2');
+    newInput.innerHTML = "<input type='text' id='editBox'>" + "<button id='newSub'>Add</button>";
+   editBtn.appendChild('newInput');
+    let replacement = document.getElementById('editBox').value;
+  }
+}
 }
 
 function todos() {
   document.getElementById("tabContent").innerHTML = "<input type='text' id='todoAdd' placeholder='To Do...'><input type='submit' id='todoB' value='Add To Do' onclick='todoList()'>";
 } 
-
-//edit btn (once coded place in correct function) also this is JUST for todos NOT THE LISTS!!!
-let editBtn = document.getElementById('editB2');
-for (let i = 0; i < editBtn.length; i++) {
-  editBtn[i].onclick = function () {
-    let newInput = document.getElementById('clickMe');
-    newInput.innerHTML = "<input type='text' id='editBox'> <input type='submit' id='newSub'>";
-    
-  }
-}
